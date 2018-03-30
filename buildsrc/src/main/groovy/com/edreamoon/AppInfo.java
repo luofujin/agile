bf54b2f97ebc7a24cc91f5da528d92b4b64e79cd
jianfeng.li <jianfeng.li@moji.com>
2018-03-30 18:39:09 +0800

    find annotated ac

---
+++ buildsrc/src/main/groovy/com/edreamoon/AppInfo.java
@@ -0,0 +1,8 @@
+package com.edreamoon;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class AppInfo {
+    public static Set<String> activitys = new HashSet<>();
+}
--- buildsrc/src/main/groovy/com/edreamoon/RouterPlu.groovy
+++ buildsrc/src/main/groovy/com/edreamoon/RouterPlu.groovy
@@ -8,5 +8,15 @@ class RouterPlu implements Plugin<Project> {
     void apply(Project project) {
         println("RouterPlu apply")
         project.android.registerTransform(new RouterTransform(project))
+        project.android.applicationVariants.all { variant ->
+            variant.outputs.each { output ->
+                output.processManifest.doLast {
+                    def manifestOutFile = output.processManifest.manifestOutputDirectory
+                    def newFileContents = manifestOutFile.getText('UTF-8')
+                    println("RouterPlu mainfest=$newFileContents")
+//                    manifestOutFile.write(newFileContents, 'UTF-8')
+                }
+            }
+        }
     }
 }
